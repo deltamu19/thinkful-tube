319a5fc4381d033efdeba10be91ff997e830f2aa
deltamu19 <daryll.martinez@gmail.com>
2016-07-13 23:33:40 -0700

    added link to videos

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $(function() {
 function getRequest(searchTerm) {
     var params = {
       'part': 'snippet',
+      'videoId': 'id',
       'key': 'AIzaSyBqOghP0DoIevyrJO1dVQBgDKKvoBKHGug',
       'q': searchTerm
     };
@@ -21,51 +22,9 @@ function getRequest(searchTerm) {
 function showResults(results) {
     var html = "";
     $.each(results, function(index, value) {
-        html += '<li>' + value.snippet.title + '<br><a href=""><img src="' + value.snippet.thumbnails.high.url + '"></a></li>';
-        console.log(value.snippet.title);
+        html += '<li>' + value.snippet.title + '<br><a target="_blank" href="https://www.youtube.com/watch?v=' + value.id.videoId + '"><img src="' + value.snippet.thumbnails.high.url + '"></a></li>';
+        console.log(value.snippet.thumbnails.high.url);
     });
     $('#search-results').show().html(html);
 }
-
-
-/*
-<img src="' + value.snippet.thumbnails.medium.url + '>
-Search for a specified string.
-var search = function () {
-    var q = $('#query').val();
-
-    var request = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet'
-    });
-
-
-    request.execute(function(response) {
-        console.log(response.result);
-        //var str = JSON.stringify(response.result);
-        $.each(response.result.items, function(index, value) {
-          console.log(value.snippet.title);
-        });
-
-        //$('#search-results').html('<pre>' + str + '</pre>');
-    });
-
-    return false;
-};
-
-function onLoadFn() {
-    // Make gapi.client calls
-    gapi.client.setApiKey('AIzaSyBqOghP0DoIevyrJO1dVQBgDKKvoBKHGug');
-
-    gapi.client.load('youtube', 'v3', function() {
-
-    });
-
-    //gapi.auth.init(function() {});
-}
-gapi.load("client", onLoadFn);
-
-$(document).ready(function() {
-  $('#search-term').submit(search);
-});
-*/
+/*need to use video ID to */
