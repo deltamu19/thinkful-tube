a1c5908fd65899c7dd598ef8cab2dbf587d0d248
deltamu19 <daryll.martinez@gmail.com>
2016-07-13 22:48:20 -0700

    update js for search-results

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
-// Search for a specified string.
+$(function() {
+    $('#search-term').submit(function(e) {
+        e.preventDefault();
+        var searchTerm = $('#input').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+    var params = {
+      'part': 'snippet',
+      'key': 'AIzaSyBqOghP0DoIevyrJO1dVQBgDKKvoBKHGug',
+      'q': searchTerm
+    };
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    $.getJSON(url, params, function(data) {
+        showResults(data.items);
+    });
+}
+
+function showResults(results) {
+    var html = "";
+    $.each(results, function(index, value) {
+        html += '<li>' + value.snippet.title + '<br><a href=""><img src="' + value.snippet.thumbnails.high.url + '"></a></li>';
+        console.log(value.snippet.title);
+    });
+    $('#search-results').show().html(html);
+}
+
+
+/*
+<img src="' + value.snippet.thumbnails.medium.url + '>
+Search for a specified string.
 var search = function () {
     var q = $('#query').val();
 
@@ -6,6 +38,7 @@ var search = function () {
         q: q,
         part: 'snippet'
     });
+
 
     request.execute(function(response) {
         console.log(response.result);
@@ -35,3 +68,4 @@ gapi.load("client", onLoadFn);
 $(document).ready(function() {
   $('#search-term').submit(search);
 });
+*/
